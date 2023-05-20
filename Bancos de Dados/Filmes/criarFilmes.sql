0504605011c4a76a7e0359029ed551128d0c4d93
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-05-20 15:54:49 -0300

    Organizando os espaçamentos e as letras maiúsculas

--- Bancos de Dados/Filmes/criarFilmes.sql
+++ Bancos de Dados/Filmes/criarFilmes.sql
@@ -1,43 +1,48 @@
 CREATE TABLE Filme (
-   	codFilme   	Serial  NOT NULL,
-   	titulo      Varchar(25),
-   	ano			integer,
-   	duracao		integer,
-   	codCateg	integer,
-   	codEst     	integer
+   	codFilme   	SERIAL         NOT NULL,
+   	titulo      VARCHAR(25),
+   	ano			INTEGER,
+   	duracao		INTEGER,
+   	codCateg	   INTEGER,
+   	codEst     	INTEGER
 );
+
 
 CREATE TABLE Artista (
-   	codArt  	Serial  NOT NULL,
-   	nomeArt		Varchar(25),
-   	cidade		Varchar(20),
-   	pais		Varchar(20),
-   	dataNasc   	Date
+   	codArt      SERIAL         NOT NULL,
+   	nomeArt	   VARCHAR(25),
+   	cidade	   VARCHAR(20),
+   	pais		   VARCHAR(20),
+   	dataNasc    DATE
 );
+
 
 CREATE TABLE Estudio (
-   	codEst 	serial  NOT NULL,
-   	nomeEst	Varchar(25)
+   	codEst 	SERIAL      NOT NULL,
+   	nomeEst	VARCHAR(25)
 );
 
+
 CREATE TABLE Categoria (
-   	codCateg   	serial  NOT NULL,
+   	codCateg   	SERIAL      NOT NULL,
    	descCateg	VARCHAR(25)
 );
 
+
 CREATE TABLE Personagem (
-   	codArt 		integer  NOT NULL,
-   	codFilme   	integer  NOT NULL,
+   	codArt 		INTEGER        NOT NULL,
+   	codFilme   	INETGER        NOT NULL,
    	nomePers  	VARCHAR(25),
-   	cache_		numeric(15,2)
+   	cache_		NUMERIC(15,2)
 );
 
-ALTER TABLE Filme 		ADD CONSTRAINT PK_Filme 			PRIMARY KEY(codFilme);
-ALTER TABLE Artista 	ADD CONSTRAINT PK_Artista 			PRIMARY KEY(codArt);
-ALTER TABLE Estudio 	ADD CONSTRAINT PK_Estudio 			PRIMARY KEY(codEst);
-ALTER TABLE Categoria 	ADD CONSTRAINT PK_Categoria 		PRIMARY KEY(codCateg);
-ALTER TABLE Personagem	ADD CONSTRAINT PK_Personagem 		PRIMARY KEY(codART,CodFilme);
-ALTER TABLE Filme 		ADD CONSTRAINT FK_Filme1Categ 		FOREIGN KEY(codCateg) 	REFERENCES Categoria;
-ALTER TABLE Filme 		ADD CONSTRAINT FK_Filme2Estud 		FOREIGN KEY(codEst) 	REFERENCES Estudio;
-ALTER TABLE Personagem 	ADD CONSTRAINT FK_Personagem2Artis	FOREIGN KEY(codArt) 	REFERENCES Artista;
-ALTER TABLE Personagem 	ADD CONSTRAINT FK_Personagem1Filme 	FOREIGN KEY(codFilme)	REFERENCES Filme;
+
+ALTER TABLE Filme 		ADD CONSTRAINT PK_Filme 			   PRIMARY KEY(codFilme);
+ALTER TABLE Artista 	   ADD CONSTRAINT PK_Artista 			   PRIMARY KEY(codArt);
+ALTER TABLE Estudio 	   ADD CONSTRAINT PK_Estudio 			   PRIMARY KEY(codEst);
+ALTER TABLE Categoria 	ADD CONSTRAINT PK_Categoria 		   PRIMARY KEY(codCateg);
+ALTER TABLE Personagem	ADD CONSTRAINT PK_Personagem 		   PRIMARY KEY(codART,CodFilme);
+ALTER TABLE Filme 		ADD CONSTRAINT FK_Filme1Categ 		FOREIGN KEY(codCateg) 	      REFERENCES Categoria;
+ALTER TABLE Filme 		ADD CONSTRAINT FK_Filme2Estud 		FOREIGN KEY(codEst) 	         REFERENCES Estudio;
+ALTER TABLE Personagem 	ADD CONSTRAINT FK_Personagem2Artis	FOREIGN KEY(codArt) 	         REFERENCES Artista;
+ALTER TABLE Personagem 	ADD CONSTRAINT FK_Personagem1Filme 	FOREIGN KEY(codFilme)	      REFERENCES Filme;
